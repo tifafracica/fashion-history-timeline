d9af00da72773918d8f14732253e1e4760a698bb
Stefanny Fracica <stefanny.fracica@avant.com>
2024-06-27 21:17:25 -0300

    vite.congif

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/fashion-history-timeline/',
   plugins: [
     remix({
+      basename: '/fashion-history-timeline/',
       ssr: false,
       future: {
         v3_fetcherPersist: true,
